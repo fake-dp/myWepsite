e4e07d534a0eb5d3554e1a2d7812dc9a7ea6b0bc
fake-dp <fakecoko92@gmail.com>
2023-03-08 17:10:49 +0900

    auto text test

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,28 +1,66 @@
 import styled from "styled-components";
-
-const HomeWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  margin: 0 auto;
-  height: 300px;
-  background-color: red;
-  overflow: hidden;
-`;
-
+import Typical from "react-typical";
 const Home = () => {
   return (
-    <>
-      <HomeWrapper>dd</HomeWrapper>
-      <HomeWrapper>dd</HomeWrapper>
-      <HomeWrapper>dd</HomeWrapper>
-      <HomeWrapper>dd</HomeWrapper>
-      <HomeWrapper>dd</HomeWrapper>
-      <HomeWrapper>dd</HomeWrapper>
-      <HomeWrapper>dd</HomeWrapper>
-    </>
+    <HomeWrapper>
+      <MyNameText>LEE JANG HE</MyNameText>
+
+      <AutoText>
+        {"  "}
+        <Typical
+          loop={Infinity}
+          wrapper="border"
+          steps={[
+            "안녕하세요! 코딩하기 좋은 날입니다~",
+            2000,
+            "저는 프론트 개발자를 꿈꾸는 이장희입니다.",
+            3000,
+            "제 웹사이트에 오신 것을 환영합니다 :)",
+            5000,
+          ]}
+        />
+      </AutoText>
+    </HomeWrapper>
   );
 };
 
 export default Home;
+
+const HomeWrapper = styled.div`
+  background-image: url("https://raw.githubusercontent.com/leejanghe/My_Website/main/my-website/public/img/intro.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const MyNameText = styled.h1`
+  font-size: 5rem;
+  font-weight: 600;
+  color: #fff;
+  text-align: center;
+  margin-bottom: 1rem;
+`;
+
+const AutoText = styled.div`
+  font-size: 2rem;
+  font-weight: 600;
+  color: #fff;
+  text-align: center;
+  margin-bottom: 1rem;
+  border-bottom: 2px solid #fff;
+  > p {
+    font-size: 2rem;
+    font-weight: 600;
+    color: #fff;
+    text-align: center;
+    margin-bottom: 1rem;
+    border-bottom: 2px solid #fff;
+  }
+`;
